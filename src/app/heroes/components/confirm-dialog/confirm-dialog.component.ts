29d3e629c45ccdf6b6c142cc6d30a07c09d94ce2
Ricardo Mera Ciudad <ricardo.mera@bosonit.com>
2023-07-06 09:09:49 +0200

    some fixes

--- src/app/heroes/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/heroes/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,7 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Hero } from '../../interfaces/hero.interface';
-import { first } from 'rxjs';
 
 @Component({
   selector: 'app-confirm-dialog',
